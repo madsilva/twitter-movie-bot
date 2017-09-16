416e8cd20517b556945b524b9cbbdaaf336b0307
Clairee Schneider <skeeter093@gmail.com>
2017-09-16 16:08:15 -0500

    Added Twitter authentication keys

--- moviebot/Search/twitter_search.py
+++ moviebot/Search/twitter_search.py
@@ -1,7 +1,7 @@
 import tweepy
 
 API_KEY = 'NGLR87Co1AT64D39ipkViztQN'
-API_SECRET = 'nxiKfdz6KLssInHVBcAK7SUWdBMysxZl6F9Xq0rwFSZxE52hV'
+API_SECRET = 'nxiKfdz6KLssInHVBcAK7SUWdBMysxZl6F9Xq0rwFSZxE52hVP'
 ACCESS_TOKEN = '909139089852649473-yXmeYbK4ib2eGs3UE2Xcw3YzR9IQH53'
 ACCESS_TOKEN_SECRET = 'bcTn7Yhd1ansZlgAkQQSCnu3Bw5NGvCSqF2lKhKlVLwme'
 
@@ -19,6 +19,7 @@ searched_tweets = []
 last_id = -1
 while len(searched_tweets) < max_tweets:
     count = max_tweets - len(searched_tweets)
+    print(1)
     try:
         new_tweets = api.search(q=query, count=count, max_id=str(last_id - 1))
         if not new_tweets:
@@ -30,3 +31,5 @@ while len(searched_tweets) < max_tweets:
         # depending on TweepError.code, one may want to retry or wait
         # to keep things simple, we will give up on an error
         break
+
+print (searched_tweets)
