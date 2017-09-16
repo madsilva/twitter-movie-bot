93421115fc412e43d564cc0b51640593c2261b28
Madeline Silva <mad.silva.ic@gmail.com>
2017-09-16 16:06:07 -0500

    gets now playing movies and adds them to context for index view

--- moviebot/bot/views.py
+++ moviebot/bot/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import tmdbsimple as tmdb
+tmdb.API_KEY = "6333e6cd17516cd6c57fcb2a4f0e4fe6"
 
 def index(request):
-    return render(request, "bot/index.html")
+    movies = tmdb.Movies()
+    now_playing_dict = movies.now_playing()['results']
+    now_playing_list = []
+    for movie in now_playing_dict:
+        now_playing_list.append(movie['title'])
+    return render(request, "bot/index.html", context={'now_playing_list':now_playing_list})
