93a9d727181c55c8e6aa6962c417474a1ea2f1e2
Madeline Silva <mad.silva.ic@gmail.com>
2017-09-17 12:59:27 -0500

    final touches

--- moviebot/bot/forms.py
+++ moviebot/bot/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class TwitterUserForm(forms.Form):
-    twitter_user = forms.ChoiceField(label='Choose a twitter user', choices=[('POTUS', 'Donald Trump (@POTUS)'), ('realdonaldtrump', 'Donald Trump (@realDonaldTrump)'), ('BarackObama', 'Barack Obama (@BarackObama)'), ])
+    twitter_user = forms.ChoiceField(label='Choose a twitter user', choices=[('POTUS', 'Donald Trump (@POTUS)'), ('realdonaldtrump', 'Donald Trump (@realDonaldTrump)'), ('BarackObama', 'Barack Obama (@BarackObama)'), ('HillaryClinton', 'Hillary Clinton (@HillaryClinton)'), ('uiowa', 'University of Iowa (@uiowa)')])
 
 
 
