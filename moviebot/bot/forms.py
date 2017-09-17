ae1e4f1c6f2292aeef20df8791f16121aaa52fd1
Madeline Silva <mad.silva.ic@gmail.com>
2017-09-17 10:30:54 -0500

    changes website and form to reflect new direction of project

--- moviebot/bot/forms.py
+++ moviebot/bot/forms.py
@@ -1,29 +1,8 @@
 from django import forms
-import tmdbsimple as tmdb
-tmdb.API_KEY = "6333e6cd17516cd6c57fcb2a4f0e4fe6"
 
+class TwitterUserForm(forms.Form):
+    twitter_user = forms.ChoiceField(choices=[('POTUS', 'Donald Trump (@POTUS)'), ])
 
-def get_movie_names():
-    movies = tmdb.Movies()
-    now_playing_dict = movies.now_playing()['results']
-    now_playing_list = []
-    for movie in now_playing_dict:
-        now_playing_list.append((movie['id'], movie['title']))
-    return now_playing_list
-
-class NowPlayingMovieForm(forms.Form):
-    movie = forms.ChoiceField(get_movie_names)
-
-def get_pop_names():
-    movies = tmdb.Movies()
-    pop_dict = movies.popular()['results']
-    pop_list = []
-    for movie in pop_dict:
-        pop_list.append((movie['id'], movie['title']))
-    return pop_list
-
-class PopularMovieForm(forms.Form):
-    movie = forms.ChoiceField(get_pop_names)
 
 
 
--- moviebot/bot/views.py
+++ moviebot/bot/views.py
@@ -1,44 +1,15 @@
 from django.shortcuts import render
-from datetime import datetime, timedelta
-import re
-from textblob import TextBlob
-import tweepy
-from tweepy import Cursor
-
-API_KEY = 'NGLR87Co1AT64D39ipkViztQN'
-API_SECRET = 'nxiKfdz6KLssInHVBcAK7SUWdBMysxZl6F9Xq0rwFSZxE52hVP'
-ACCESS_TOKEN = '909139089852649473-yXmeYbK4ib2eGs3UE2Xcw3YzR9IQH53'
-ACCESS_TOKEN_SECRET = 'bcTn7Yhd1ansZlgAkQQSCnu3Bw5NGvCSqF2lKhKlVLwme'
-
+from .forms import TwitterUserForm
 
 def index(request):
     context = {}
-
-    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-
-
-    date = datetime.today()
-    end_date = datetime.date(day=1, month=1, year=2017)
-    sentiment_analysis_vals = {}
-    while date > end_date:
-        query = date
-        max_tweets = 100
-        search = api.search(q=query, count=max_tweets)
-        sum = 0
-        count = 0
-        try:
-            for tweet in search:
-                clean_tweet = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
-                analysis = TextBlob(clean_tweet)
-                sum += analysis.sentiment.polarity
-                count+= 1
-        except tweepy.TweepError as e:
-            print(e)
-
-        date = date - timedelta(day=1)
-
-
-
+    if ('submit' in request.GET):
+        user = request.GET['twitter_user']
+        context['twitter_user'] = user
+        context['form'] = TwitterUserForm(request.GET)
+        context['user_image_url'] = "https://twitter.com/" + user + "/profile_image?size=original"
+        context['graph_url'] = "static/bot/" + user + ".jpg"
+        # do something with user to get image
+    else:
+        context['form'] = TwitterUserForm()
     return render(request, "bot/index.html", context)
